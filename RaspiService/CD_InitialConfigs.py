ac66045b332a0874215d6aefa2d64d48b2985160
santi2208 <santi2208@gmail.com>
2018-09-15 15:19:19 -0300

    Agrego CountDown
    Agrego un programa de Cuenta regresiva.
    CD_Run.py

---
+++ RaspiService/CD_InitialConfigs.py
@@ -0,0 +1,55 @@
+from Numero import Numero
+from Servo import Servo
+from ServoPos import ServoPos
+
+class CD_InitialConfigs(object):
+    def __init__(self):
+        self.Servos = self.SetServos()
+        self.Posiciones = self.SetPosiciones()
+    
+    def SetNumbers(self):
+        numbers = []
+        i = 0
+        for pos in self.Posiciones:
+            numbers.append(self.SetNumber(i + 1, "Numero {}".format(i), pos))
+            i = i + 1
+        
+        return numbers
+    
+    def SetNumber(self, numberId, numberName, positions):
+        pos = 0
+        servosPos = []
+        for servo in self.Servos:
+            servosPos.append(ServoPos(servo, positions[pos]))
+            pos = pos + 1
+        return Numero(numberId, numberName, servosPos)
+    
+    def SetFinalCharacter(self):
+        fPos = [1, 0, 1, 0, 1, 1, 0] # C 
+        return self.SetNumber("CAMBIO", "Caracter Finalizado F", fPos)
+
+    def SetPosiciones(self):
+        posiciones = []
+        # posiciones.append([1, 1, 1, 1, 1, 1, 0]) # 0
+        posiciones.append([0, 1, 0, 1, 0, 0, 0]) # 1
+        posiciones.append([1, 1, 0, 0, 1, 1, 1]) # 2
+        posiciones.append([1, 1, 0, 1, 1, 0, 1]) # 3
+        posiciones.append([0, 1, 1, 1, 0, 0, 1]) # 4
+        posiciones.append([1, 0, 1, 1, 1, 0, 1]) # 5
+        posiciones.append([1, 0, 1, 1, 1, 1, 1]) # 6
+        posiciones.append([1, 1, 0, 1, 0, 0, 0]) # 7
+        posiciones.append([1, 1, 1, 1, 1, 1, 1]) # 8
+        posiciones.append([1, 1, 1, 1, 1, 0, 1]) # 9
+        return posiciones
+    
+    def SetServos(self):
+        servos = []
+        servos.append(Servo('Deg0', 'Deg180', 2)) #SRV1
+        servos.append(Servo('Deg180', 'Deg90', 3)) #SRV2
+        servos.append(Servo('Deg90', 'Deg180', 4)) #SRV3
+        servos.append(Servo('Deg180', 'Deg90', 14)) #SRV4
+        servos.append(Servo('Deg180', 'Deg90', 15)) #SRV5
+        servos.append(Servo('Deg90', 'Deg180', 18)) #SRV6
+        servos.append(Servo('Deg180', 'Deg90', 23)) #SRV7
+        return servos
+        
--- RaspiService/CD_Run.py
+++ RaspiService/CD_Run.py
@@ -2,7 +2,7 @@ import sys
 import time
 import argparse
 from ServoActions import *
-from InitialConfigs import *
+from CD_InitialConfigs import *
 from decimal import Decimal
 from MainServoController import *
 from HttpProxy import HttpProxy
@@ -12,19 +12,25 @@ class MainServoCounter(object):
         self.httpProxy = httpProxy 
         self.servoActions = servoActions
         self.numeros = initialConfig.SetNumbers()
+        self.finalChar = initialConfig.SetFinalCharacter()
         self.Implementation = implementation if implementation is not None else 1
-        self.numeroFijo = numero 
-        self.SleepTime = sleepTime if sleepTime is not None else 2
+        self.desde = numero 
+        self.SleepTime = sleepTime 
         self.MainServoController = MainServoController()
 
     def RunService(self):
-        print("Iniciando...")
-        nums = self.numeros if self.numeroFijo is None else [self.numeros[self.numeroFijo]]
-        for num in nums:
+        while True:
+            print("Iniciando...")
+            nums = self.numeros if self.desde is None else self.numeros[:self.desde]
+            for num in reversed(nums):
+                if(self.httpProxy is not None):
+                    self.InformState(num)
+                self.SetNumber(num)
+                time.sleep(self.SleepTime)
             if(self.httpProxy is not None):
-                self.InformState(num)
-            self.SetNumber(num)
-            time.sleep(self.SleepTime)
+                self.InformState(self.finalChar)
+            self.SetNumber(self.finalChar)
+            time.sleep(5)
 
     def SetNumber(self, num):
         print("-------NUMERO {} --------".format(num.Nombre))
@@ -43,15 +49,15 @@ class MainServoCounter(object):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--all', help='0: Un servo a la vez || 1: Todos los servos al mismo tiempo - Default: 1')
-    parser.add_argument('--num', help='Numero Fijo - Default: 0 - 9')
+    parser.add_argument('--num', help='Numero Desde - Default: 5 to 1.')
     parser.add_argument('--time', help='Sleep Time: Intervalo de espera entre numero y numero - Default: 0.8')
-    parser.add_argument('--web', help='URL servidor al que reportar estado - Default: None')
+    parser.add_argument('--sIp', help='Ip del servidor.')
     parser.add_argument('--useWeb', help='Reporta estado a un servidor - Default: False')
     args = parser.parse_args()    
     
     implementation = int(args.all) if args.all is not None else 1
-    sleepTime = Decimal(args.time) if args.time is not None else 0.8
-    numero = int(args.num) if args.num is not None else None
-    httpProxy = HttpProxy(args.web) if args.web is not None else HttpProxy('http://192.168.0.18:8012/SetCounterState')
-    sc = MainServoCounter(httpProxy if args.useWeb is not None else None, ServoActions(), InitialConfigs(), implementation, sleepTime, numero)
+    sleepTime = Decimal(args.time) if args.time is not None else 59
+    numero = int(args.num) if args.num is not None else 5
+    httpProxy = HttpProxy('http://{}:8012/SetCounterState'.format(args.sIp)) if args.sIp is not None else HttpProxy('http://192.168.0.18:8012/SetCounterState')
+    sc = MainServoCounter(httpProxy if args.useWeb is not None else None, ServoActions(), CD_InitialConfigs(), implementation, sleepTime, numero)
     sc.RunService()
